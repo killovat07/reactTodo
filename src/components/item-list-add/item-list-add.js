67e1bff320ee84c3ff8da810a51542cddabc6646
Dmitry <dfkillovat@gmail.com>
2019-08-05 16:48:11 +0300

    add form

--- src/components/item-list-add/item-list-add.js
+++ src/components/item-list-add/item-list-add.js
@@ -4,15 +4,38 @@ import './item-list-add.css';
 
 export default class ItemListAdd extends Component {
 
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value.toUpperCase()
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddItem(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
   render() {
-    const { onAddItem } = this.props;
 
     return (
+      <form className="item-add-form d-flex" onSubmit={ this.onSubmit }>
+        <input type="text" 
+         className="form-control"
+         onChange={this.onLabelChange}
+         placeholder="New todo" 
+         value={ this.state.label } />
         <button type="button"
-                className="btn btn-primary btn-lg float-right"
-                onClick={ () => onAddItem('Hello world') }>
-          <i className="fa fa-plus" />
+                className="btn btn-primary btn-md float-right">
+        Add Item
         </button>
+      </form>
     );
   };
 }
